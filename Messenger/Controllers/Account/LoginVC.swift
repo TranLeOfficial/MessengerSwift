169c79899935390b95f89ab116420bfbf4ff166c
TranLeOfficial <tranthanhnhank123@gmail.com>
2021-09-27 19:48:17 +0700

    commit

--- Messenger/Controllers/Account/LoginVC.swift
+++ Messenger/Controllers/Account/LoginVC.swift
@@ -7,23 +7,49 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
 
+class LoginVC: UIViewController {
+    
+    //MARK: - object View
+    private let imageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "messenger")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    
+    //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Login"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTapRegister))
+        //add subView
+        addSubView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size = view.frame.size.width / 3
+        let sizeWidth = view.frame.size.width
+        imageView.frame = CGRect(x: (sizeWidth - size) / 2,
+                                 y: size,
+                                 width: size,
+                                 height: size)
     }
-    */
+    
+    private func addSubView() {
+        view.addSubview(imageView)
+    }
+    
+    @objc private func didTapRegister() {
+        let vc = RegisterVC()
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
