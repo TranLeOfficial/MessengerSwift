5c17d0c836ead472b44edb99e78e753b74304140
TranLeOfficial <tranthanhnhank123@gmail.com>
2021-09-27 15:33:02 +0700

    reset main

--- Messenger/Controllers/ViewController.swift
+++ Messenger/Controllers/ViewController.swift
@@ -12,20 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Tran Thanh Nhan")
-        print("NhanTT13")
-        print("Merge lan 4")
-        //merge NhanTT13 branch
-        print("Merge lan 5")
-//<<<<<<< HEAD
-//
-//=======
-        print("Master")
-//>>>>>>> NhanTT13
-
-//=======
-        print("Master NhanTT14")
-        print("Nguyen Van NhanTT14")
-//>>>>>>> NhanTT14
     }
 
 
