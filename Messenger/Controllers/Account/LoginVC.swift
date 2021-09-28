7175f1ca13258495d1270384c766e6899be57b98
TranLeOfficial <tranthanhnhank123@gmail.com>
2021-09-28 11:10:04 +0700

    commit with xcode

--- Messenger/Controllers/Account/LoginVC.swift
+++ Messenger/Controllers/Account/LoginVC.swift
@@ -11,12 +11,66 @@ import UIKit
 class LoginVC: UIViewController {
     
     //MARK: - object View
+    //imageView
     private let imageView : UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "messenger")
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
+    //scrollView
+    private let scrollView : UIScrollView =  {
+       let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+       return scrollView
+    }()
+    //textField
+    private let emailField : UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Email address..."
+        //padding
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        return field
+    }()
+    private let passwordField : UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .done
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Password..."
+        //padding
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    //Button
+    private let loginButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
+    }()
+    
 
     
     //MARK: - Life cycle
@@ -32,19 +86,40 @@ class LoginVC: UIViewController {
         //add subView
         addSubView()
     }
-    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let size = view.width / 3
-        let sizeWidth = view.width
+        positionObjectView()
+    }
+    
+    //set position view
+    private func positionObjectView() {
+        scrollView.frame = view.bounds
+        let size = scrollView.width / 3
+        let sizeWidth = scrollView.width
         imageView.frame = CGRect(x: (sizeWidth - size) / 2,
                                  y: size,
                                  width: size,
                                  height: size)
+        emailField.frame = CGRect(x: 30,
+                                 y: imageView.bottom + 20,
+                                 width: scrollView.width - 60,
+                                 height: 50)
+        passwordField.frame = CGRect(x: 30,
+                                 y: emailField.bottom + 10,
+                                 width: scrollView.width - 60,
+                                 height: 50)
+        loginButton.frame = CGRect(x: 30,
+                                 y: passwordField.bottom + 20,
+                                 width: scrollView.width - 60,
+                                 height: 50)
     }
     
     private func addSubView() {
-        view.addSubview(imageView)
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
     }
     
     @objc private func didTapRegister() {
