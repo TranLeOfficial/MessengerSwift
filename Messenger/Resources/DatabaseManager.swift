b31daa3ead61568c819dccbd20b80ce596c220af
TranLeOfficial <tranthanhnhank123@gmail.com>
2021-09-29 22:30:22 +0700

    format email

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -19,8 +19,10 @@ extension DatabaseManager {
     
     public func userExists(with email: String,
                            completion: @escaping ((Bool) -> Void)) {
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
         
-        database.child(email).observeSingleEvent(of: .value) { snapshot in
+        database.child(safeEmail).observeSingleEvent(of: .value) { snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
                 return
@@ -31,7 +33,7 @@ extension DatabaseManager {
     
     //Add new user to Database
     public func insertUser(with user: ChatAppUser) {
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName,
             "password": user.password
@@ -45,5 +47,11 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let password: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
     //let profilePictureUrl: String
 }
